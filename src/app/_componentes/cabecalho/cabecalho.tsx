c82a9921ba5d59216f223977207b280a1be1500c
Gabriel Bello <gassis574@gmail.com>
2024-08-07 15:50:35 -0300

    Update - Mudança na home e no cabeçalho

--- src/app/_componentes/cabecalho/cabecalho.tsx
+++ src/app/_componentes/cabecalho/cabecalho.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import Image from "next/image";
 import Logo from "../logo.png";
@@ -5,103 +7,160 @@ import { SocialIcon } from "react-social-icons";
 import { Button } from "@/components/ui/button";
 import { HiOutlineMail } from "react-icons/hi";
 import { BsTelephoneFill } from "react-icons/bs";
-import { Separator } from "@/components/ui/separator"
+import { Separator } from "@/components/ui/separator";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuGroup,
   DropdownMenuItem,
-  DropdownMenuSub,
-  DropdownMenuSubContent,
-  DropdownMenuSubTrigger,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import Link from "next/link";
 
-const Cabecalho = () => {
+const HeaderContact = () => {
   return (
-    <div className="bg-orange-900 text-orange-50 px-4 py-2 border-b border-slate-700">
-      <div className="flex flex-col sm:flex-row items-center justify-between">
-        {/* Seção da logo */}
-        <div className="flex items-center justify-center">
-          <Image src={Logo} alt="Logo JPSystem" />
-        </div>
-        {/* Seção de contato */}
-        <div className="flex flex-row-reverse items-center mt-0 sm:mt-0 sm:absolute sm:left-1/2 sm:transform sm:-translate-x-1/2 gap-8">
+    <div className="flex justify-center">
+      {/* Seção da logo */}
+      <div className="bg-orange-900">
+        <Image src={Logo} alt="Logo JPSystem" />
+      </div>
+      {/* Parte laranja */}
+      <div className="bg-orange-900 text-orange-50 px-4 py-2 flex-1 flex items-center justify-center">
+        <div className="flex items-center gap-8">
           <div className="flex items-center gap-2">
-          <BsTelephoneFill className="fill-white" />
+            <BsTelephoneFill className="text-orange-50" />
             <span>(11)95481-2706</span>
           </div>
           <div className="flex items-center gap-2">
-            <HiOutlineMail />
+            <HiOutlineMail className="text-orange-50" />
             <span>jpsystem@gmail.com</span>
           </div>
         </div>
-        {/* Seção de ícones de mídia social */}
-        <div className="flex space-x-2 mr-2 sm:mt-0">
+      </div>
+      {/* Divisor
+      <div className="bg-black w-1"></div> */}
+      {/* Parte cinza escuro */}
+      <div className="bg-orange-900 text-orange-50 px-4 py-2 flex-1 flex items-center justify-end">
+        <div className="flex space-x-2">
           <SocialIcon
+            bgColor="#00431407"
             style={{ height: 25, width: 25 }}
             url="https://www.linkedin.com/company/jpsystem/"
           />
         </div>
       </div>
-      {/* Seção de navegação */}
-      <nav className="flex justify-center space-x-4 mt-2">
-        <Button className="text-lg hover:bg-transparent" variant="ghost">
-          Home
-        </Button>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button className="text-lg hover:bg-transparent text-slate-50" variant="ghost">
-              Serviços
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent className="w-56 text-lg text-slate-800 border-none opacity-80">
-            <DropdownMenuGroup>
-              <DropdownMenuItem>Consultoria de TI</DropdownMenuItem>
-              <Separator className="my-2" />
-              <DropdownMenuItem>Desenvolvimento</DropdownMenuItem>
-              <Separator className="my-2" />
-              <DropdownMenuItem>Software com serviço</DropdownMenuItem>
-            </DropdownMenuGroup>
-          </DropdownMenuContent>
-        </DropdownMenu>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button className="text-lg hover:bg-transparent" variant="ghost">
-              Blog
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent className="w-56 bg-orange-900 border-none text-orange-50">
-            <DropdownMenuGroup>
-              <DropdownMenuItem>News</DropdownMenuItem>
-              <Separator className="my-2" />
-              <DropdownMenuItem>Domine TI</DropdownMenuItem>
-              <Separator className="my-2" />
-              <DropdownMenuItem>Reflexões</DropdownMenuItem>
-            </DropdownMenuGroup>
-          </DropdownMenuContent>
-        </DropdownMenu>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button className="text-lg hover:bg-transparent" variant="ghost">
-              Quem Somos
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent className="w-56 bg-orange-900 border-none text-orange-50">
-            <DropdownMenuGroup>
-              <DropdownMenuItem>Sobre a JPSystem</DropdownMenuItem>
-              <Separator className="my-2" />
-              <DropdownMenuItem>Nossos pilares</DropdownMenuItem>
-              <Separator className="my-2" />
-              <DropdownMenuItem>Tecnologia</DropdownMenuItem>
-            </DropdownMenuGroup>
-          </DropdownMenuContent>
-        </DropdownMenu>
-        <Button className="text-lg hover:bg-transparent" variant="ghost">
-          Contato
-        </Button>
-      </nav>
     </div>
+  );
+};
+
+const Cabecalho = () => {
+  return (
+    <>
+      {/* Header de Contato */}
+      <HeaderContact />
+      {/* Header Principal */}
+      <div className="bg-orange-900 text-slate-50 px-4 py-2 border-b border-slate-700">
+        <div className="flex items-center justify-center">
+          {/* Seção de navegação */}
+          <nav className="flex space-x-4 items-center">
+            <Link href="/home" passHref>
+              <Button className="text-lg hover:bg-transparent" variant="ghost">
+                Home
+              </Button>
+            </Link>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button className="text-lg hover:bg-transparent" variant="ghost">
+                  Serviços
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-56 bg-orange-900 border-none text-slate-50">
+                <DropdownMenuGroup>
+                  <Link href="/servicos/consultoria-de-ti" passHref>
+                    <DropdownMenuItem asChild>
+                      <span>Consultoria de TI</span>
+                    </DropdownMenuItem>
+                  </Link>
+                  <Separator className="my-2" />
+                  <Link href="/servicos/desenvolvimento" passHref>
+                    <DropdownMenuItem asChild>
+                      <span>Desenvolvimento</span>
+                    </DropdownMenuItem>
+                  </Link>
+                  <Separator className="my-2" />
+                  <Link href="/servicos/software-com-servico" passHref>
+                    <DropdownMenuItem asChild>
+                      <span>Software com serviço</span>
+                    </DropdownMenuItem>
+                  </Link>
+                </DropdownMenuGroup>
+              </DropdownMenuContent>
+            </DropdownMenu>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button className="text-lg hover:bg-transparent" variant="ghost">
+                  Blog
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-56 bg-orange-900 border-none text-slate-50">
+                <DropdownMenuGroup>
+                  <Link href="/blog/news" passHref>
+                    <DropdownMenuItem asChild>
+                      <span>News</span>
+                    </DropdownMenuItem>
+                  </Link>
+                  <Separator className="my-2" />
+                  <Link href="/blog/domine-ti" passHref>
+                    <DropdownMenuItem asChild>
+                      <span>Domine TI</span>
+                    </DropdownMenuItem>
+                  </Link>
+                  <Separator className="my-2" />
+                  <Link href="/blog/reflexoes" passHref>
+                    <DropdownMenuItem asChild>
+                      <span>Reflexões</span>
+                    </DropdownMenuItem>
+                  </Link>
+                </DropdownMenuGroup>
+              </DropdownMenuContent>
+            </DropdownMenu>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button className="text-lg hover:bg-transparent" variant="ghost">
+                  Quem Somos
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-56 bg-orange-900 border-none text-slate-50">
+                <DropdownMenuGroup>
+                  <Link href="/quem-somos/sobre" passHref>
+                    <DropdownMenuItem asChild>
+                      <span>Sobre a JPSystem</span>
+                    </DropdownMenuItem>
+                  </Link>
+                  <Separator className="my-2" />
+                  <Link href="/quem-somos/pilares" passHref>
+                    <DropdownMenuItem asChild>
+                      <span>Nossos pilares</span>
+                    </DropdownMenuItem>
+                  </Link>
+                  <Separator className="my-2" />
+                  <Link href="/quem-somos/tecnologia" passHref>
+                    <DropdownMenuItem asChild>
+                      <span>Tecnologia</span>
+                    </DropdownMenuItem>
+                  </Link>
+                </DropdownMenuGroup>
+              </DropdownMenuContent>
+            </DropdownMenu>
+            <Link href="/contato" passHref>
+              <Button className="text-lg hover:bg-transparent" variant="ghost">
+                Contato
+              </Button>
+            </Link>
+          </nav>
+        </div>
+      </div>
+    </>
   );
 };
 
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,8 +1,213 @@
+"use client";
+
+import React, { useEffect, useCallback, useState } from "react";
+import useEmblaCarousel from "embla-carousel-react";
+import Autoplay from "embla-carousel-autoplay";
+import Image from "next/image";
+import { BsTelephoneInbound } from "react-icons/bs";
+import { MdEmail } from "react-icons/md";
+import Image1 from "@/app/_componentes/Imagemdenuvem.jpg";
+import Image2 from "@/app/_componentes/Saas.png";
+import BannerImage from "@/app/_componentes/Imagemdenuvem.jpg";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 export default function Home() {
+  const [emblaRef, emblaApi] = useEmblaCarousel(
+    { loop: true },
+    [Autoplay({ delay: 8000 })] // Autoplay a cada 8 segundos
+  );
+
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  const onSelect = useCallback(() => {
+    if (emblaApi) {
+      setSelectedIndex(emblaApi.selectedScrollSnap());
+    }
+  }, [emblaApi]);
+
+  useEffect(() => {
+    if (emblaApi) {
+      emblaApi.on("select", onSelect);
+      onSelect(); // Inicializa o índice selecionado
+    }
+    return () => {
+      if (emblaApi) {
+        emblaApi.off("select", onSelect);
+      }
+    };
+  }, [emblaApi, onSelect]);
+
+  const slides = [Image1, Image2]; // Use as imagens importadas
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div><h1>JP System Ltda</h1>
-      <h2>Home</h2>
+    <main className="flex min-h-screen flex-col mt-2">
+      {/* Começo do carousel */}
+      <div
+        className="relative w-[calc(80%-40px)] px-4 mx-auto border-2 border-slate-900"
+        style={{ marginBottom: "calc(50px + 10vh)" }}
+      >
+        <div className="embla" ref={emblaRef}>
+          <div className="embla__container flex">
+            {slides.map((src, index) => (
+              <div
+                key={index}
+                className="flex flex-shrink-0 w-full h-[300px] relative"
+              >
+                <Image
+                  src={src}
+                  alt={`Slide ${index}`}
+                  fill
+                  style={{ objectFit: "cover" }}
+                  sizes="100vw"
+                />
+              </div>
+            ))}
+          </div>
+          <button
+            className="absolute top-1/2 left-8 transform -translate-y-1/2 z-10 p-2 text-2xl text-black rounded-full"
+            onClick={() => emblaApi && emblaApi.scrollPrev()}
+            aria-label="Previous slide"
+          >
+            &lt;
+          </button>
+          <button
+            className="absolute top-1/2 right-8 transform -translate-y-1/2 z-10 p-2 text-2xl text-black rounded-full"
+            onClick={() => emblaApi && emblaApi.scrollNext()}
+            aria-label="Next slide"
+          >
+            &gt;
+          </button>
+        </div>
+        <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex space-x-2">
+          {slides.map((_, index) => (
+            <button
+              key={index}
+              type="button"
+              className={`h-3 w-3 rounded-full ${
+                index === selectedIndex ? "bg-orange-500" : "bg-gray-300"
+              }`}
+              aria-label={`Go to slide ${index + 1}`}
+              onClick={() => emblaApi && emblaApi.scrollTo(index)}
+            />
+          ))}
+        </div>
+      </div>
+      {/* Fim do carousel */}
+      {/* Começo Seção conteúdo */}
+      <div className="border-2 border-slate-800 m-6 bg-orange-50">
+        <div className="flex flex-row items-center justify-between ml-12 mr-6 mt-11 mb-6">
+          <div className="flex flex-col w-1/2">
+            <h1 className="text-orange-900 text-5xl text-left">
+              What is Lorem Ipsum?
+            </h1>
+            <span className="mt-6 text-left mr-6 text-orange-800">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum.
+            </span>
+          </div>
+          <div className="relative border-2 border-slate-800 shadow-lg rounded-lg overflow-hidden w-1/3 h-[300px] mr-6">
+            <Image
+              src={Image1}
+              alt="Imagem"
+              className="object-cover w-full h-full"
+            />
+          </div>
+        </div>
+        <div className="flex items-center mt-6 mb-6">
+          <span className="text-2xl text-orange-900 mr-4 relative ml-6">
+            Divisor
+            <div className="absolute left-full top-1/2 transform -translate-y-1/2 mt-0.5 ml-4 h-1 w-10 bg-orange-800" />
+          </span>
+        </div>
+        <div className="mb-4">
+          <div className="flex flex-row flex-wrap gap-6 justify-center mr-6">
+            <Card className="w-[350px]">
+              <CardHeader>
+                <div className="flex flex-col space-y-1.5"></div>
+                <Image
+                  src={Image1}
+                  alt="Imagem"
+                  className="object-cover w-full h-full"
+                />
+              </CardHeader>
+              <CardContent>
+                <CardTitle>Titulo Card</CardTitle>
+                <CardDescription>Conteúdo Card</CardDescription>
+              </CardContent>
+              <CardFooter className="flex justify-between"></CardFooter>
+            </Card>
+            <Card className="w-[350px]">
+              <CardHeader>
+                <div className="flex flex-col space-y-1.5"></div>
+                <Image
+                  src={Image1}
+                  alt="Imagem"
+                  className="object-cover w-full h-full"
+                />
+              </CardHeader>
+              <CardContent>
+                <CardTitle>Titulo Card</CardTitle>
+                <CardDescription>Conteúdo Card</CardDescription>
+              </CardContent>
+              <CardFooter className="flex justify-between"></CardFooter>
+            </Card>
+            <Card className="w-[350px]">
+              <CardHeader>
+                <div className="flex flex-col space-y-1.5"></div>
+                <Image
+                  src={Image1}
+                  alt="Imagem"
+                  className="object-cover w-full h-full"
+                />
+              </CardHeader>
+              <CardContent>
+                <CardTitle>Titulo Card</CardTitle>
+                <CardDescription>Conteúdo Card</CardDescription>
+              </CardContent>
+              <CardFooter className="flex justify-between"></CardFooter>
+            </Card>
+          </div>
+        </div>
+      </div>
+      {/* Banner Contato */}
+      <div className="flex items-center p-4 shadow-lg rounded-lg mt-6 mx-6 w-[50%] bg-orange-50 border-2 border-slate-900 justify-center mb-4">
+        <div className="flex items-center justify-center w-12 h-12 bg-red-500 rounded-full mr-4">
+          <BsTelephoneInbound className="text-white w-6 h-6" />
+        </div>
+        <div className="flex flex-col">
+          <span className="text-gray-700 text-sm">
+            Entre em contato conosco
+          </span>
+          <span className="text-blue-900 text-2xl font-bold mr-6">
+            (11)95481-2706
+          </span>
+        </div>
+        <div className="flex items-center justify-center w-12 h-12 bg-red-500 rounded-full mr-4">
+          <MdEmail className="text-white w-6 h-6" />
+        </div>
+        <div className="flex flex-col">
+          <span className="text-gray-700 text-sm">
+            Entre em contato conosco
+          </span>
+          <span className="text-blue-900 text-2xl font-bold">
+            jpsystem@gmail.com
+          </span>
+        </div>
       </div>
     </main>
   );
